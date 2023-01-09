5abea99ca869c60e3e015160bcc3ac2f111d1829
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2023-01-09 13:46:12 +0300

    set the modal

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,4 +1,9 @@
 import { defineStore } from "pinia";
 export default defineStore("modal", {
   state: () => ({ isOpen: false }),
+  getters: {
+    hiddenClass(state) {
+      return !state.isOpen ? "hidden" : "";
+    },
+  },
 });
